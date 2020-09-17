4aa1e28ff279ebba89d37d346ea9e0f278e900fc
Lord_Arnor <aronlex@gmail.com>
2020-09-17 10:39:51 +0000

    betra printPosibleDirection

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,21 +1,23 @@
-curentPlace = "32"
+curentPlace = "12"
 
 def printPosibleDirections (placement):
-    print("You can travel: ")
+
+    writeLine = "You can travel: "
     if not(placement[1] == "3"):
         if not(placement[0] == "2" and placement[1] == "2"):
-            print("(N)orth")
+            writeLine += "(N)orth "
     if not(placement[0] == "3"):
         if not(placement[0] == "1" and placement[1] == "1"):
             if not(placement[0] == "2" and placement[1] == "2"):
                 if not(placement[0] == "2" and placement[1] == "1"):
-                    print("(E)ast")
+                    writeLine += "(E)ast "
     if not(placement[1] == "1"):
         if not(placement[0] == "2" and placement[1] == "3"):
-            print("(S)outh")
+            writeLine += "(S)outh "
     if not(placement[0] == "1"):
         if not(placement[0] == "2" and placement[1] == "1"):
             if not(placement[0] == "3" and placement[1] == "1"):
                 if not(placement[0] == "3" and placement[1] == "2"):
-                    print("(W)east")
+                    writeLine += "(W)east "
+    print(writeLine)
 printPosibleDirections(curentPlace)
