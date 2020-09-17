a2e00a160b7dbd9e0ef8a07713aa4d07cec05e26
Lord_Arnor <aronlex@gmail.com>
2020-09-17 10:10:32 +0000

    Secound Commit

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,1 +1,12 @@
-Print("hello")
+curentPlace = "23"
+
+def printPosibleDirections (placement):
+    print("You can travel: ")
+    if(placement[1] == "1"):
+        print("(N)orth")
+    if(placement[0] == "1" and not placement[1] == "1"):
+        print("(E)ast")
+    if not(placement[1] == "1" and (placement[0] == 2 and placement[0] == 3)):
+        print("(S)outh")
+
+printPosibleDirections(curentPlace)
