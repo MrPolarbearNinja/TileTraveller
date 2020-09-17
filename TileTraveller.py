b2b24025627fa6e62ad4980a8f724420faff5301
Lord_Arnor <aronlex@gmail.com>
2020-09-17 10:30:44 +0000

    finesd printPosibleDirection

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,12 +1,21 @@
-curentPlace = "23"
+curentPlace = "32"
 
 def printPosibleDirections (placement):
     print("You can travel: ")
-    if(placement[1] == "1"):
-        print("(N)orth")
-    if(placement[0] == "1" and not placement[1] == "1"):
-        print("(E)ast")
-    if not(placement[1] == "1" and (placement[0] == 2 and placement[0] == 3)):
-        print("(S)outh")
-
+    if not(placement[1] == "3"):
+        if not(placement[0] == "2" and placement[1] == "2"):
+            print("(N)orth")
+    if not(placement[0] == "3"):
+        if not(placement[0] == "1" and placement[1] == "1"):
+            if not(placement[0] == "2" and placement[1] == "2"):
+                if not(placement[0] == "2" and placement[1] == "1"):
+                    print("(E)ast")
+    if not(placement[1] == "1"):
+        if not(placement[0] == "2" and placement[1] == "3"):
+            print("(S)outh")
+    if not(placement[0] == "1"):
+        if not(placement[0] == "2" and placement[1] == "1"):
+            if not(placement[0] == "3" and placement[1] == "1"):
+                if not(placement[0] == "3" and placement[1] == "2"):
+                    print("(W)east")
 printPosibleDirections(curentPlace)
